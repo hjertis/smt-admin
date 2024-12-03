74fc16e9d710b0d747fa2490f466ab9478e6c7d8
Michael <michael.hjertstrom@gmail.com>
2024-12-03 08:55:51 +0100

    Update Planning.jsx
    Holy shit, Calendar is working properly with modals! Still missing Drag and Drop though....

--- src/pages/Planning.jsx
+++ src/pages/Planning.jsx
@@ -1,5 +1,15 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Stack,
+} from "@mui/material";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 import { collection, getDocs } from "firebase/firestore";
@@ -10,6 +20,9 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 export default function Planning(props) {
   const database = collection(db, "newOrders");
   const [documents, setDocuments] = React.useState([]);
+  const [selectedEvent, setSelectedEvent] = React.useState();
+  const [modalState, setModalState] = React.useState(false);
+  const [orderInformation, setOrderInformation] = React.useState({});
 
   React.useEffect(() => {
     const getDocuments = async () => {
@@ -23,6 +36,12 @@ export default function Planning(props) {
     };
     getDocuments();
   }, []);
+
+  const handleSelectedEvent = (event) => {
+    setOrderInformation(documents.find((document) => document.id === event.id));
+    setSelectedEvent(event);
+    setModalState(true);
+  };
 
   const ColoredDateCellWrapper = ({ children }) =>
     React.cloneElement(React.Children.only(children), {
@@ -53,21 +72,55 @@ export default function Planning(props) {
       <Calendar
         localizer={momentLocalizer(moment)}
         events={documents.map((document) => ({
-          title: document.orderNumber,
+          title: document.orderNumber + " - " + document.description,
           start: document.start.toDate(),
           end: document.end.toDate(),
           id: document.id,
+          allDay: true,
         }))}
         components={components}
         defaultDate={defaultDate}
         views={views}
         max={max}
         showMultiDayTimes
+        showAllEvents
         step={60}
         startAccessor={"start"}
         endAccessor={"end"}
-        style={{ height: 1200, width: 1600 }}
+        style={{ height: 1600, width: 1600 }}
+        onSelectEvent={(e) => handleSelectedEvent(e)}
       />
+      <Dialog open={modalState} onClose={() => setModalState(false)}>
+        <DialogTitle>Order Information</DialogTitle>
+        <DialogContent>
+          <Stack spacing={2}>
+            <p>Order Number: {orderInformation.orderNumber}</p>
+            <p>Description: {orderInformation.description}</p>
+            <p>Part No: {orderInformation.partNo}</p>
+            <p>Quantity: {orderInformation.quantity}</p>
+            <p>
+              Start:{" "}
+              {!orderInformation.start
+                ? ""
+                : orderInformation.start.toDate().toLocaleString()}
+            </p>
+            <p>
+              End:{" "}
+              {!orderInformation.end
+                ? ""
+                : orderInformation.end.toDate().toLocaleString()}
+            </p>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <ButtonGroup variant="contained">
+            <Button color="error" onClick={() => setModalState(false)}>
+              Close
+            </Button>
+            <Button color="success">Save</Button>
+          </ButtonGroup>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
