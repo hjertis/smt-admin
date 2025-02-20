596db38d0abfb0a94b08c6af67ed6b91d1cd942e
Michael <michael.hjertstrom@gmail.com>
2025-02-20 12:21:01 +0100

    Syncfusion

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { registerLicense } from "@syncfusion/ej2-base";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Register Syncfusion license
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWX1ccHRTR2VYUUVzXUY="
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
